59414204abaa51809677b4984b40e938dce74e94
Jesse Squires <jesse.d.squires@gmail.com>
2015-05-17 13:38:26 -0700

    finish initial view layout and loading url. update demo.

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -21,7 +21,22 @@ import UIKit
 import JSQWebViewController
 
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+
+        let webViewController = WebViewController(url: NSURL(string: "http://jessesquires.com")!)
+
+        switch indexPath.row {
+        case 0:
+            self.navigationController?.pushViewController(webViewController, animated: true)
+            break
+        case 1:
+            break
+        default: break
+        }
+    }
 
 }
 
--- JSQWebViewController/JSQWebViewController/WebViewController.swift
+++ JSQWebViewController/JSQWebViewController/WebViewController.swift
@@ -19,10 +19,59 @@
 import UIKit
 import WebKit
 
-class WebViewController: UIViewController {
+public class WebViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    public lazy var webView: WKWebView = {
+        let webView = WKWebView(frame: self.view.bounds, configuration: self.configuration)
+        self.view.addSubview(webView)
+        return webView
+    }()
+
+    public lazy var progressBar: UIProgressView = {
+        let progressBar = UIProgressView(progressViewStyle: .Bar)
+        self.view.addSubview(progressBar)
+        return progressBar
+    }()
+
+    public var urlRequest: NSURLRequest {
+        didSet {
+            webView.loadRequest(urlRequest)
+        }
     }
+
+    private let configuration: WKWebViewConfiguration
+
+    public init(urlRequest: NSURLRequest, configuration: WKWebViewConfiguration = WKWebViewConfiguration()) {
+        self.urlRequest = urlRequest
+        self.configuration = configuration
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    public convenience init(url: NSURL) {
+        self.init(urlRequest: NSURLRequest(URL: url))
+    }
+
+    public required init(coder aDecoder: NSCoder) {
+        self.urlRequest = NSURLRequest(URL: NSURL(string: "")!)
+        self.configuration = WKWebViewConfiguration()
+        super.init(coder: aDecoder)
+    }
+
+    // MARK: View lifecycle
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        webView.loadRequest(urlRequest)
+        progressBar.setProgress(0.5, animated: true)
+    }
+
+    public override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.webView.frame = self.view.bounds
+        self.view.bringSubviewToFront(self.progressBar)
+        self.progressBar.frame = CGRect(x: 0, y: self.topLayoutGuide.length, width: self.view.frame.size.width, height: 2)
+    }
+
+
 
 }
