75d60e9483f5259d729f0a87eba7e6163fc52939
Jesse Squires <jesse.d.squires@gmail.com>
2015-05-17 12:12:46 -0700

    add example project and workspace

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  Created by Jesse Squires
+//  http://www.jessesquires.com
+//
+//
+//  Documentation
+//  http://www.jessesquires.com/JSQWebViewController
+//
+//
+//  GitHub
+//  https://github.com/jessesquires/JSQWebViewController
+//
+//
+//  License
+//  Copyright (c) 2015 Jesse Squires
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
